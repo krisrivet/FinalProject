1453ff36cfc2e3d6efd6aebcf5f20e79a0b38cb6
krisrivet <krivet@academic.rrc.ca>
2018-03-22 00:29:03 +0000

    Show's are not working still

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   resource :basketballs, only: [:index, :show]
   resource :clothes, only: [:index, :show]
   resource :gears, only: [:index, :show]
   resource :jerseys, only: [:index, :show]
   resource :shoes, only: [:index, :show]
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
