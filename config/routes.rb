fa8a5d8941c62137d0515ecce099ff288d592b7e
krisrivet <krivet@academic.rrc.ca>
2018-03-22 15:49:59 +0000

    Shows work now in Shoesgit add .! TY KYLE!

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resource :basketballs, only: [:index, :show]
-  resource :clothes, only: [:index, :show]
-  resource :gears, only: [:index, :show]
-  resource :jerseys, only: [:index, :show]
-  resource :shoes, only: [:index, :show]
+  resources :basketballs, only: [:index, :show]
+  resources :clothes, only: [:index, :show]
+  resources :gears, only: [:index, :show]
+  resources :jerseys, only: [:index, :show]
+  resources :shoes, only: [:index, :show]
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
