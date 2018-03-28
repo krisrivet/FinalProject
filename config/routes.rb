7509fb698d250ec04547281f1e28ffa278264242
krisrivet <krivet@academic.rrc.ca>
2018-03-28 02:17:38 +0000

    Created the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home_page/show'
+
   resources :basketballs, only: [:index, :show]
   resources :clothes, only: [:index, :show]
   resources :gears, only: [:index, :show]
@@ -8,5 +10,6 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  root to: "home_page#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
